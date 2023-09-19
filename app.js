ee4d8097ff92fa10ef0637c032610852a76fec34
raedmokni <raedmokni2@gmail.com>
2023-09-19 20:40:35 +0100

    add comments

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-// // Starting with node js using console
-
-// console.log("Starting our node ..");
-
-// function sum (a,b){
-//     return a+b
-// }
-
-// console.log(sum(5,6));
+// Import the HTTP module to create an HTTP server
 const http = require("http");
 
+// Create an HTTP server and define a callback function to handle requests
 const server = http.createServer(function (request, response) {
+  // Check if the request URL is "/home" and the method is GET
   if (request.url === "/home" && request.method === "GET") {
-    response.write("<h1>welcome Home</h1>");
+    // If the condition is met, send a welcome message as a response
+    response.write("<h1>Welcome Home</h1>");
     response.end();
   } else if (request.url === "/addData" && request.method === "POST") {
+    // If the request URL is "/addData" and the method is POST
+    // Handle incoming data from the request
     request.on("data", (data) => {
-      console.log(data);
-      response.statusCode = 201
-      response.end('data recieved')
+      console.log(data); // Log the received data
+      response.statusCode = 201; // Set the response status code to 201 (Created)
+      response.end('Data received'); // Send a response indicating that data was received
     });
   } else {
+    // If none of the above conditions are met, set the response status code to 404 (Not Found)
     response.statusCode = 404;
-    response.end("url not found");
+    response.end("URL not found");
   }
 });
 
+// Start the server and listen on port 5200
 server.listen(5200, (error) => {
   if (error) {
-    console.log("error!", error);
+    console.log("Error!", error); // Log an error message if there's an error starting the server
   } else {
-    console.log("we are running on port 5200..");
+    console.log("Server is running on port 5200.."); // Log a success message if the server starts successfully
   }
 });
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+// Importing the Express.js library
 const express = require("express")
 
+// Defining the port number on which the server will listen
 const port = 5100
+
+// Creating an Express application instance
 const app = express()
 
-
-app.get("/welcome",(req,res)=>{
+// Setting up a route for handling GET requests to "/welcome"
+app.get("/welcome", (req, res) => {
+    // Sending a response with a greeting message
     res.send("Hello bro ✌️😍")
 })
 
-app.listen(port,()=>{
-    console.log(`we are running on express ${port}..`);
+// Starting the Express server and listening on the specified port
+app.listen(port, () => {
+    // Logging a message to indicate that the server is running
+    console.log(`Server is running on port ${port}..`);
 })
